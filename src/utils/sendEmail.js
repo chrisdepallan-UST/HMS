13ddd1dd65b7dc7153adda8911dd7499555093f1
chrisdepallan-UST <308389@ust.com>
2026-05-08 12:01:50 +0530

    thats what i like

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,29 +1,31 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-   host: "smtp.gmail.com",
-  service: "gmail",
-  port: 587,
-  secure: false, // TLS
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },family: 4,
-    debug: true,
-  logger: true,
-  connectionTimeout: 30000,
-  greetingTimeout : 30000,
-  socketTimeout: 30000
-});
-
-const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-    
+const sendEmail = async ({ firstName, lastName, email, message }) => {
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "myEmail@gmail.com",
+      pass: "password",
+    },
+    secure: true,
   });
+
+  await transporter.verify();
+
+  const mailData = {
+    from: {
+      name: `${firstName} ${lastName}`,
+      address: "myEmail@gmail.com",
+    },
+    replyTo: email,
+    to: "recipient@gmail.com",
+    subject: `form message`,
+    text: message,
+    html: `${message}`,
+  };
+
+  await transporter.sendMail(mailData);
 };
 
 module.exports = sendEmail;
