b2bce31b9553460006b5bbd5ae7242b715e47e38
chrisdepallan-UST <308389@ust.com>
2026-05-08 11:36:37 +0530

    change in mailer

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,19 +1,23 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-   host: "smtp.gmail.com",
-  service: "gmail",
+  host: "smtp.gmail.com",
   port: 587,
-  secure: false, // TLS
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },family: 4,
-    debug: true,
-  logger: true,
-  connectionTimeout: 30000,
-  greetingTimeout : 30000,
-  socketTimeout: 30000
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false,
+    family: 4,
+  },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
